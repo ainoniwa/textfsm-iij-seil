b08d94ef94fbf9fa85bc45784cf2701390768a8f
ruy <ruy@ainoniwa.net>
2018-04-14 19:53:01 +0900

    Add templates ARP and NAT
    * iij_seil_show_status_arp.template
    * iij_seil_show_status_nat.template

---
+++ tests/test_parse.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import os
+import json
+import textfsm
+
+
+def parse_and_diff_correct_dict(target):
+    base = os.path.dirname(os.path.abspath(__file__))
+    text_path = os.path.normpath(os.path.join(base, '{s}/{s}.raw'.format(s=target)))
+    correct_path = os.path.normpath(os.path.join(base, '{s}/{s}.json'.format(s=target)))
+    template_path = os.path.normpath(os.path.join(base, '../templates/{s}.template'.format(s=target)))
+    with open(text_path) as f:
+        text = f.read()
+
+    with open(template_path) as f:
+        t = textfsm.TextFSM(f)
+        pt = t.ParseText(text)
+        parsed_dict = [dict(zip(t.header, l)) for l in pt]
+        correct_dict = json.load(open(correct_path))
+        assert parsed_dict == correct_dict
+
+
+def test_iij_seil_show_status_arp():
+    parse_and_diff_correct_dict("iij_seil_show_status_arp")
+
+
+def test_iij_seil_show_status_nat():
+    parse_and_diff_correct_dict("iij_seil_show_status_nat")
